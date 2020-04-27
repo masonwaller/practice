79ceb51bb610ac9560ca43f48e31aaea10113e11
github username <github email address>
2020-04-27 11:54:55 -0700

    Put a map on the page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from "google-maps-react"
 
-function App() {
+
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Map
+        google={props.google}
+        zoom={9}
+        center={{
+          lat: "50",
+          lng: "50"
+        }}>
+      </Map>
     </div>
   );
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: process.env.REACT_APP_MAPS_API_KEY
+})(App);
