fa48e451d3ecdbafb13a95838219172647af1384
github username <github email address>
2020-05-31 12:49:02 -0700

    put a graph using chartjs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Pad from './pad.js'
+import Graph from './graph.js'
 
 export default function App(props) {
   return (
     <div className="App">
       <Pad/>
+      <Graph />
     </div>
   );
 }
---
+++ src/graph.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Line} from 'react-chartjs-2';
+
+export default function Graph(props) {
+    const [chartData, setData] = React.useState({})
+    const chart = () => {
+        setData({labels: ['monday', 'turesday', 'wednesday'],
+            datasets: [{label: 'level of thicccness',
+                data: [64, 57, 26],
+                backgroundColor: [ 'rgba(74, 57, 24, 78)'],
+                borderWidth: 4
+            }]
+        })
+    }
+    React.useEffect(() => {
+        chart()
+    }, [])
+
+    return (
+      <div>
+        <Line data={chartData}/>
+      </div>
+    );
+  }
