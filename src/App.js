63edf010152527f0c49fd579c0ce76b68019f653
github username <github email address>
2020-06-01 11:28:11 -0700

    made graph data changeable

--- src/App.js
+++ src/App.js
@@ -4,10 +4,11 @@ import Pad from './pad.js'
 import Graph from './graph.js'
 
 export default function App(props) {
+  const [values, setValues] = [0]
   return (
     <div className="App">
       <Pad/>
-      <Graph />
+      <Graph values={values}/>
     </div>
   );
 }
--- src/graph.js
+++ src/graph.js
@@ -4,9 +4,9 @@ import {Line} from 'react-chartjs-2';
 export default function Graph(props) {
     const [chartData, setData] = React.useState({})
     const chart = () => {
-        setData({labels: ['monday', 'turesday', 'wednesday'],
-            datasets: [{label: 'level of thicccness',
-                data: [64, 57, 26],
+        setData({labels: ['0', '1', '2'],
+            datasets: [{label: 'Graph',
+                data: [props.values[0], (props.values[1])? props.values[1]:props.values[0], (props.values[2])? props.values[2]:props.values[0] ],
                 backgroundColor: [ 'rgba(7, 59, 5, 3)'],
                 borderWidth: 4
             }]
